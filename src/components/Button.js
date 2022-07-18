ac3649206c4ebef4f28f7c33a9e91069ff407a19
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-07-17 18:12:23 -0700

    add base, confirm, danger, onClick & disable conditionals for Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,21 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const { onClick, disabled, children } = props
+
+   let buttonClass = "button";
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+    }
+
+    if (props.danger) {
+      buttonClass += " button--danger";
+    }
+
+
+   return(
+   <button onClick={onClick} className={buttonClass} disabled={disabled}>
+      {children}
+   </button>
+   );
 }
