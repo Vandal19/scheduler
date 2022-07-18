752bb1716782d7b51bd7eae4caef48bcde6d1b9c
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-07-18 16:36:30 -0700

    create DayList component

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "components/DayList.scss";
+import classNames from "classnames";
+
+export default function DayList(props) {
+
+  return (
+  <ul>
+    
+  </ul>
+  )
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,6 +5,16 @@ import classNames from "classnames";
 export default function DayListItem(props) {
   const { setDay, name, selected, spots } = props
 
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return `no spots remaining`;
+    } else if (spots === 1) {
+      return `1 spot remaining`;
+    } else {
+      return `${spots} spots remaining`;
+    }
+  };
+
   let dayClass = classNames("day-list__item", {
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0
@@ -13,7 +23,7 @@ export default function DayListItem(props) {
   return (
     <li onClick={() => setDay(name)} className={dayClass}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
