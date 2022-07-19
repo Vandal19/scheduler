24e524a4264e87e13295f5d1d6bfcbffcfa3aa18
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-07-19 16:35:36 -0700

    setup diretory & storybook for Appointment component with index.js as default module

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 
+import Appointment from "components/Appointment/index.js"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
@@ -130,5 +132,19 @@ storiesOf("InterviewerList", module)
     <InterviewerList
       interviewers={interviewers}
       onChange={action("SetInterviewer")}
+    />
+  ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => (
+    <Appointment
+    />
+  ))
+  .add("Appointment with Time", () => (
+    <Appointment
+      time="12pm"
     />
   ));
