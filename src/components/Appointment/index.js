7042fd68160d90f013f59571bc097472b5e4e3ef
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-07-23 18:15:40 -0700

    Implement custom Hooks to interface with diff navigation of Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,22 +4,34 @@ import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode"
 
 
 export default function Appointment(props) {
-  const { time, interview, student, interviewer } = props
+  const { time, interview, interviewers } = props
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE"
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
   return(
     <article className="appointment">
       <Header time={time}/>
-        {interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={interview.student}
           interviewer={interview.interviewer}
         />
-        :
-        <Empty />
-        }
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back(EMPTY)}
+        />
+      )}
     </article>
   );
 }
