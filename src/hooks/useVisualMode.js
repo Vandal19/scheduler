cb7129b837f060452052e44a41696b12e429c269
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-07-23 17:38:42 -0700

    Building a custom Hook to mange visual mode of any component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initial){
+  const [ mode, setMode ] = useState(initial)
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function(newMode, replace = false) {
+    setMode(newMode)
+    setHistory(prev =>
+      replace ?
+      [...prev.slice(0,-1), newMode]
+      :
+      [...prev, newMode]
+      )
+  }
+
+  const back = () => {
+    const newHistory = [...history]
+    newHistory.pop()
+    setHistory(newHistory)
+    newHistory.length > 1
+    ? setMode(newHistory[newHistory.length -1])
+    : setMode(initial)
+  }
+
+
+return {mode, transition, back}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const transition = function(newMode, replace = false) {
+  //   setMode(newMode)
+  //   setHistory(prev =>
+  //     replace ?
+  //     [...prev.slice(0, -1), newMode]
+  //     :
+  //     [...prev, newMode]
+  //   )
+  // };
+
+  // const back = () => {
+  //   const newHistory = [...history];
+  //   // console.log(newHistory)
+  //   console.log([...history])
+  //   newHistory.pop();
+  //   console.log(newHistory)
+  //   setHistory(newHistory)
+  //   newHistory.length > 1 ? setMode(newHistory[newHistory.length - 1]) : setMode(initial)
+  // };
